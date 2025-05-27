bf10c3c0f077fc7544017b10f1c8de1c327a8dfe
epxzzy <epxzzy66business@outlook.com>
2025-05-27 21:25:10 +0530

    update (custom model)

--- run2/main.js
+++ run2/main.js
@@ -2,233 +2,84 @@ import * as THREE from 'three';
 
 import Stats from 'three/addons/libs/stats.module.js';
 
-let SCREEN_WIDTH = window.innerWidth;
-let SCREEN_HEIGHT = window.innerHeight;
-let aspect = SCREEN_WIDTH / SCREEN_HEIGHT;
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { RoomEnvironment } from 'three/addons/environments/RoomEnvironment.js';
 
-let container, stats;
-let camera, scene, renderer, mesh;
-let cameraRig, activeCamera, activeHelper;
-let cameraPerspective, cameraOrtho;
-let cameraPerspectiveHelper, cameraOrthoHelper;
-const frustumSize = 600;
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
 
-init();
+let mixer;
 
-function init() {
+const clock = new THREE.Clock();
+const container = document.getElementById( 'container' );
 
-  container = document.createElement( 'div' );
-  document.body.appendChild( container );
+const stats = new Stats();
+container.appendChild( stats.dom );
 
-  scene = new THREE.Scene();
-  //scene.background = new THREE.Color(0x00FF00)
+const renderer = new THREE.WebGLRenderer( { antialias: true } );
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight );
+container.appendChild( renderer.domElement );
 
-  //
+const pmremGenerator = new THREE.PMREMGenerator( renderer );
 
-  camera = new THREE.PerspectiveCamera( 50, 0.5 * aspect, 1, 10000 );
-  camera.position.z = 2500;
+const scene = new THREE.Scene();
+scene.background = new THREE.Color( 0xbfe3dd );
+scene.environment = pmremGenerator.fromScene( new RoomEnvironment(), 0.04 ).texture;
 
-  cameraPerspective = new THREE.PerspectiveCamera( 50, 0.5 * aspect, 150, 1000 );
+const camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 100 );
+camera.position.set( 5, 2, 8 );
 
-  cameraPerspectiveHelper = new THREE.CameraHelper( cameraPerspective );
-  scene.add( cameraPerspectiveHelper );
+const controls = new OrbitControls( camera, renderer.domElement );
+controls.target.set( 0, 0.5, 0 );
+controls.update();
+controls.enablePan = false;
+controls.enableDamping = false;
 
-  //
-  cameraOrtho = new THREE.OrthographicCamera( 0.5 * frustumSize * aspect / - 2, 0.5 * frustumSize * aspect / 2, frustumSize / 2, frustumSize / - 2, 150, 1000 );
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath( './gltf/' );
 
-  cameraOrthoHelper = new THREE.CameraHelper( cameraOrtho );
-  scene.add( cameraOrthoHelper );
+const loader = new GLTFLoader();
+loader.setDRACOLoader( dracoLoader );
+loader.load( './assets/buildings.glb', function ( gltf ) {
 
-  //
+  const model = gltf.scene;
+  //model.position.set( 1, 1, 0 );
+  //model.scale.set( 0.01, 0.01, 0.01 );
+  scene.add( model );
 
-  activeCamera = cameraPerspective;
-  activeHelper = cameraPerspectiveHelper;
+  //mixer = new THREE.AnimationMixer( model );
+  //mixer.clipAction( gltf.animations[ 0 ] ).play();
 
-
-  // counteract different front orientation of cameras vs rig
-
-  cameraOrtho.rotation.y = Math.PI;
-  cameraPerspective.rotation.y = Math.PI;
-
-  cameraRig = new THREE.Group();
-
-  cameraRig.add( cameraPerspective );
-  cameraRig.add( cameraOrtho );
-
-  scene.add( cameraRig );
-
-  //
-
-  mesh = new THREE.Mesh(
-    new THREE.SphereGeometry( 100, 16, 8 ),
-    new THREE.MeshBasicMaterial( { color: 0xffffff, wireframe: true } )
-  );
-  scene.add( mesh );
-
-  const mesh2 = new THREE.Mesh(
-    new THREE.SphereGeometry( 50, 16, 8 ),
-    new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe: true } )
-  );
-  mesh2.position.y = 150;
-  mesh.add( mesh2 );
-
-  const mesh3 = new THREE.Mesh(
-    new THREE.SphereGeometry( 5, 16, 8 ),
-    new THREE.MeshBasicMaterial( { color: 0x0000ff, wireframe: true } )
-  );
-  mesh3.position.z = 150;
-  cameraRig.add( mesh3 );
-
-  //
-
-  const geometry = new THREE.BufferGeometry();
-  const vertices = [];
-
-  for ( let i = 0; i < 10000; i ++ ) {
-
-    vertices.push( THREE.MathUtils.randFloatSpread( 2000 ) ); // x
-    vertices.push( THREE.MathUtils.randFloatSpread( 2000 ) ); // y
-    vertices.push( THREE.MathUtils.randFloatSpread( 2000 ) ); // z
-
-  }
-
-  geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
-
-  const particles = new THREE.Points( geometry, new THREE.PointsMaterial( { color: 0x888888 } ) );
-  scene.add( particles );
-
-  //
-
-  renderer = new THREE.WebGLRenderer( { antialias: true } );
-  renderer.setPixelRatio( window.devicePixelRatio );
-  renderer.setSize( SCREEN_WIDTH, SCREEN_HEIGHT );
   renderer.setAnimationLoop( animate );
-  container.appendChild( renderer.domElement );
 
-  renderer.setScissorTest( true );
+}, undefined, function ( e ) {
 
-  //
+    console.error( e );
 
-  stats = new Stats();
-  container.appendChild( stats.dom );
+  } );
 
-  //
 
-  window.addEventListener( 'resize', onWindowResize );
-  document.addEventListener( 'keydown', onKeyDown );
+window.onresize = function () {
 
-}
-
-//
-
-function onKeyDown( event ) {
-
-  switch ( event.keyCode ) {
-
-    case 79: /*O*/
-
-      activeCamera = cameraOrtho;
-      activeHelper = cameraOrthoHelper;
-
-      break;
-
-    case 80: /*P*/
-
-      activeCamera = cameraPerspective;
-      activeHelper = cameraPerspectiveHelper;
-
-      break;
-
-  }
-
-}
-
-//
-
-function onWindowResize() {
-
-  SCREEN_WIDTH = window.innerWidth;
-  SCREEN_HEIGHT = window.innerHeight;
-  aspect = SCREEN_WIDTH / SCREEN_HEIGHT;
-
-  renderer.setSize( SCREEN_WIDTH, SCREEN_HEIGHT );
-
-  camera.aspect = 0.5 * aspect;
+  camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
 
-  cameraPerspective.aspect = 0.5 * aspect;
-  cameraPerspective.updateProjectionMatrix();
+  renderer.setSize( window.innerWidth, window.innerHeight );
 
-  cameraOrtho.left = - 0.5 * frustumSize * aspect / 2;
-  cameraOrtho.right = 0.5 * frustumSize * aspect / 2;
-  cameraOrtho.top = frustumSize / 2;
-  cameraOrtho.bottom = - frustumSize / 2;
-  cameraOrtho.updateProjectionMatrix();
+};
 
-}
-
-//
 
 function animate() {
 
-  render();
+  const delta = clock.getDelta();
+
+  //mixer.update( delta );
+
+  controls.update();
+
   stats.update();
 
-}
-
-
-function render() {
-
-  const r = Date.now() * 0.0005;
-
-  mesh.position.x = 700 * Math.cos( r );
-  mesh.position.z = 700 * Math.sin( r );
-  mesh.position.y = 700 * Math.sin( r );
-
-  mesh.children[ 0 ].position.x = 70 * Math.cos( 2 * r );
-  mesh.children[ 0 ].position.z = 70 * Math.sin( r );
-
-  if ( activeCamera === cameraPerspective ) {
-
-    cameraPerspective.fov = 35 + 30 * Math.sin( 0.5 * r );
-    cameraPerspective.far = mesh.position.length();
-    cameraPerspective.updateProjectionMatrix();
-
-    cameraPerspectiveHelper.update();
-    cameraPerspectiveHelper.visible = true;
-
-    cameraOrthoHelper.visible = false;
-
-  } else {
-
-    cameraOrtho.far = mesh.position.length();
-    cameraOrtho.updateProjectionMatrix();
-
-    cameraOrthoHelper.update();
-    cameraOrthoHelper.visible = true;
-
-    cameraPerspectiveHelper.visible = false;
-
-  }
-
-  cameraRig.lookAt( mesh.position );
-
-  //
-
-  activeHelper.visible = false;
-
-  renderer.setClearColor( 0x000000, 1 );
-  renderer.setScissor( 0, 0, SCREEN_WIDTH / 2, SCREEN_HEIGHT );
-  renderer.setViewport( 0, 0, SCREEN_WIDTH / 2, SCREEN_HEIGHT );
-  renderer.render( scene, activeCamera );
-
-  //
-
-  activeHelper.visible = true;
-
-  renderer.setClearColor( 0x111111, 1 );
-  renderer.setScissor( SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2, SCREEN_HEIGHT );
-  renderer.setViewport( SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2, SCREEN_HEIGHT );
   renderer.render( scene, camera );
 
 }
